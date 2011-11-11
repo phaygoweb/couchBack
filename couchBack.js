a7a867c7689284bc368fcc078c1345351bc56dc3
Kyle A. Matheny <mathenk2+phaygoweb@gmail.com>
2011-11-11 16:35:57 -0500

    spacing

--- couchBack.js
+++ couchBack.js
@@ -1,6 +1,6 @@
 var http = require('http'),
     couchBack = {
-    
+
         // Defaults for couch connection
         'couch': {
             'host': 'localhost',
@@ -11,55 +11,59 @@ var http = require('http'),
 
         'connect': (function () {
             'use strict';
-            
+
             var args = {};
-            
+
             return function () {
                 // Set local variable to function arguments
                 args = arguments;
-                
+
                 // Arguments passed
                 if (args[0] !== undefined) {
                     // Host
-                    couchBack.couch.host = 
+                    couchBack.couch.host =
                         (args[0].host === undefined)
-                        ? couchBack.couch.host 
+                        ? couchBack.couch.host
                         : args[0].host;
-                    
+
                     // Port
                     couchBack.couch.port =
                         (args[0].port === undefined)
                         ? couchBack.couch.port
                         : args[0].port;
-                        
+
                     // Username
-                    couchBack.couch.username = 
+                    couchBack.couch.username =
                         (args[0].username === undefined)
                         ? couchBack.couch.username
                         : args[0].username;
-                    
+
                     // Password
-                    couchBack.couch.password = 
+                    couchBack.couch.password =
                         (args[0].password === undefined)
                         ? couchBack.couch.password
                         : args[0].password;
                 }
-                
+
                 console.log(couchBack.couch);
-                
+
                 var req = http.request({
                         host: couchBack.couch.host,
                         port: couchBack.couch.port,
                         path: '/_session',
                         method: 'GET'
-                    }, function(res) {
+                    }, function (res) {
                         console.log('Status:', res.statusCode);
                     });
-                    
+
                 req.end();
-                
+
             };
-            
+
+        }()),
+
+        'createDoc': (function () {
+            'use strict';
         }())
 
     };
