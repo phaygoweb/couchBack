99017286d1c0a3c43fc540670d6615ddcfc609de
Kyle A. Matheny <mathenk2+phaygoweb@gmail.com>
2011-11-13 11:12:38 -0500

    remake .init

--- couchBack.js
+++ couchBack.js
@@ -14,7 +14,10 @@ var http = require('http'),
             'use strict';
 
             var args = {},
-                req;
+                argKeys,
+                argLen,
+                req,
+                temp;
 
             return function () {
                 // Set local variable to function arguments
@@ -22,63 +25,43 @@ var http = require('http'),
 
                 // Arguments passed
                 if (args[0] !== undefined) {
-                    // Host
-                    couchBack.couch.host =
-                        (args[0].host === undefined)
-                        ? couchBack.couch.host
-                        : args[0].host;
+                    argKeys = Object.keys(args[0]);
+                    argLen = argKeys.length;
 
-                    // Port
-                    couchBack.couch.port =
-                        (args[0].port === undefined)
-                        ? couchBack.couch.port
-                        : args[0].port;
+                    while (argLen--) {
+                        // Temporary key storage
+                        temp = argKeys[argLen];
 
-                    // Username
-                    couchBack.couch.username =
-                        (args[0].username === undefined)
-                        ? couchBack.couch.username
-                        : args[0].username;
+                        couchBack.couch[temp] =
+                            (args[0][temp] === undefined)
+                            ? couchBack.couch[temp]
+                            : args[0][temp];
+                    }
 
-                    // Password
-                    couchBack.couch.password =
-                        (args[0].password === undefined)
-                        ? couchBack.couch.password
-                        : args[0].password;
+                    console.log(couchBack.couch);
                 }
-
-                console.log(couchBack.couch);
-
-                req = http.request({
-                        host: couchBack.couch.host,
-                        port: couchBack.couch.port,
-                        path: '/',
-                        method: 'GET'
-                    }, function (res) {
-                        console.log(res.statusCode);
-                        if (res.statusCode === 200) {
-                            couchBack.couch.established = true;
-                        } else {
-                            couchBack.couch.established = false;
-                        }
-                        
-                        //console.log(couchBack.couch);
-                    });
-                
-                req.on('error', function(e) {
-                    console.log('Problem with request:', e.message);
-                });
-
-                req.end();
             };
 
+        }()),
+
+        'request': (function () {
+            'use strict';
+
+            return function () {
+
+            };
         }()),
 
         'createDoc': (function () {
             'use strict';
 
             return function () {
+                console.log(couchBack.couch.established);
+                if (!couchBack.couch.established) {
+                    return false;
+                }
 
+                console.log('createDoc');
             };
         }())
 
